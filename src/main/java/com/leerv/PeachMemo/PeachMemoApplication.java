54dc5acc5a039a72cef83ac239e2566937a24486
Leerv474 <roman.lee474@gmail.com>
2024-12-23 23:04:27 +0300

    init

---
+++ src/main/java/com/leerv/PeachMemo/PeachMemoApplication.java
@@ -0,0 +1,13 @@
+package com.leerv.PeachMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeachMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leerv/PeachMemo/PeachMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leerv.PeachMemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
